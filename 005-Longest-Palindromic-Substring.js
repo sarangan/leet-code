61fe0fdd7539ed3743a333febc2de4c811db784c
拖米 <lishuo.lishuo@alibaba-inc.com>
2018-12-17 19:43:32 +0800

    [005] Runtime: 80 ms, faster than 93.80% of JavaScript online submissions for Longest Palindromic Substring.

--- 005-Longest-Palindromic-Substring.js
+++ 005-Longest-Palindromic-Substring.js
@@ -1,21 +1,18 @@
 function spreadCheck(arr, left, right) {
   const [bottom, up] = [0, arr.length - 1];
   let l = left, r = right;
-  if (arr[l] !== arr[r]) return '';
+  if (arr[l] !== arr[r]) return [0, 0];
 
-  let result = l === r
-    ? [arr[l]]
-    : [arr[l], arr[r]];
-
-  while (--l >= bottom && ++r <= up) {
-    if (arr[l] === arr[r]) {
-      result.push(arr[r]);
-      result.unshift(arr[l]);
-    } else {
-      break;
-    }
+  while (--l >= bottom & ++r <= up && arr[l] === arr[r]) {
+    // console.log(arr[l], arr[r]);
   }
-  return result.join('');
+  return [l + 1, r - 1];
+}
+
+function compareLength(tuple0, tuple1) {
+  const l0 = tuple0[1] - tuple0[0];
+  const l1 = tuple1[1] - tuple1[0];
+  return l0 > l1 ? tuple0 : tuple1;
 }
 
 /**
@@ -24,14 +21,15 @@ function spreadCheck(arr, left, right) {
  */
 var longestPalindrome = function(s) {
   const sa = s.toString().split('');
-  return sa
+  const [l, r] = sa
     .map((_, i) => {
       const n = spreadCheck(sa, i, i);
       const n1 = spreadCheck(sa, i, i + 1);
-      return n.length > n1.length ? n : n1;
+      return compareLength(n, n1);
     })
-    .reduce((p, n) => p.length > n.length ? p : n, '');
+    .reduce((p, n) => compareLength(p, n), [0, 0]);
+  return s.substring(l, r + 1);
 };
 
 
-console.log(longestPalindrome("avaa"));
+console.log(longestPalindrome("aava"));
