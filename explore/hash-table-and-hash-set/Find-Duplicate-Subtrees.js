b334651846c5db2d2067a71155df38fc3b9305e1
tomi <lishuo.tomi@gmail.com>
2019-01-02 01:34:48 +0800

    [hash-table-and-hash-set]

--- explore/hash-table-and-hash-set/Find-Duplicate-Subtrees.js
+++ explore/hash-table-and-hash-set/Find-Duplicate-Subtrees.js
@@ -1,50 +1,22 @@
-const { TreeNode, createTreeFronString } = require('../../tools');
+const { BinaryTreeNode, createTreeFronString } = require('../../tools');
 
-var serialize = function(root) {
-  if (!root) return '[]';
-  const stack = [root];
-  let i = 0;
-  while (stack[i] !== undefined) {
-    if (stack[i] === null) {
-      stack.push(null);
-      i++;
-      continue;
-    }
-    stack.push(stack[i].left);
-    stack.push(stack[i].right);
-    i++;
-  }
-  return '[' + stack.map(e => e ? e.val : 'null').toString() + ']';
-};
-
-/**
- * @param {TreeNode} root
- * @return {TreeNode[]}
- */
 var findDuplicateSubtrees = function(root) {
-  if(!root) return [];
-  const obj = {};
-
-  const traverse = node => {
-    if(!node) return;
-    const key = serialize(node);
-    if (obj[key] === undefined) obj[key] = [];
-    obj[key].push(node);
-
-    if (node.left) traverse(node.left);
-    if (node.right) traverse(node.right);
-  };
-  traverse(root);
-
-  const results = [];
-  for (const s of Object.keys(obj)) {
-    if (obj[s].length > 1) {
-      results.push(obj[s][0]);
-    }
-  }
-  return results;
+  let obj = {}, res = [];
+  preOrder(root, obj, res);
+  return res;
 };
 
-const data = createTreeFronString('[1,2,3,4,null,2,4,null,null,4]');
-// console.log(data);
+function preOrder(root, map, res) {
+  if (root === null) return '#';
+  let str = root.val + preOrder(root.left, map, res) + preOrder(root.right, map, res);
+  if (!map[str]) map[str] = 0;
+  map[str]++;
+  if (map[str] === 2) res.push(root);
+  return str;
+}
+
+
+const data = createTreeFronString('[0,0,0,0,null,null,0,null,null,null,0]');
+
+console.log(data);
 console.log(findDuplicateSubtrees(data));
--- tools/index.js
+++ tools/index.js
@@ -8,6 +8,10 @@ function BinaryTreeNode(val) {
   this.left = this.right = null;
 }
 
+BinaryTreeNode.prototype.toString = function() {
+  return `BTN: v:${this.val};l:${this.left};r:${this.right}`;
+};
+
 function LinkedBinaryTreeNode(val) {
   this.val = val;
   this.left = this.right = this.next = null;
@@ -48,19 +52,40 @@ function getbtn(val) {
   return new BinaryTreeNode(val);
 }
 
-function createTreeFronString(data) {
+function findHeight(target) {
+  let l = 0, r = target / 2;
+  while (l + 1 < r) {
+    let mid = l + Math.floor((r - l) / 2);
+    if (2 ** mid < target && 2 ** (mid + 1) > target) return mid;
+    if (2 ** (mid - 1) < target && 2 ** mid > target) return mid - 1;
+    if (2 ** (mid + 1) >= target) r = mid;
+    if (2 ** (mid - 1) <= target) l = mid;
+  }
+}
+
+
+function createTreeFromString(data) {
   const arr = data
     .substring(1, data.length - 1)
     .split(',')
     .filter(e => e)
-    .map(e => e === 'null' ? null : new TreeNode(+e));
+    .map(e => e === 'null' ? null : new BinaryTreeNode(+e));
   if (arr.length === 0) return null;
-  for (let i = 1; i <= arr.length; i++) {
+
+  //find height with binary search
+  const height = findHeight(arr.length);
+  for (let i = 1; i < 2 ** (height - 1); i++) {
     if (arr[2 * i]) arr[i - 1].left = arr[2 * i];
     if (arr[2 * i + 1]) arr[i - 1].right = arr[2 * i + 1];
   }
-  arr[0].left = arr[1];
-  arr[0].right = arr[2];
+  const sub = arr.slice(2 ** height - 1, arr.length);
+  for (let i = 2 ** (height - 1) - 1; i < 2 ** height - 1; i++) {
+    if (arr[i - 1] !== null) {
+      arr[i - 1].left = sub.shift();
+      arr[i - 1].right = sub.shift();
+      //console.log(arr[i - 1]);
+    }
+  }
   return arr[0];
 }
 
@@ -73,6 +98,6 @@ module.exports = {
   linkListToArray,
   TreeNode,
   getbtn,
-  createTreeFronString,
+  createTreeFronString: createTreeFromString,
   logger: Logger(),
 };
