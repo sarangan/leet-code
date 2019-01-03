346c6f05a80f5474a0c4c1e382cda0915de5d2b8
tomi <lishuo.tomi@gmail.com>
2019-01-03 23:13:47 +0800

    [BST]

--- explore/binary-search-tree/Delete-Node-in-a-BST.js
+++ explore/binary-search-tree/Delete-Node-in-a-BST.js
@@ -61,23 +61,29 @@ const findSuccessor = (target) => {
  * @param {number} key
  * @return {TreeNode}
  */
-var deleteNode = function (root, key) {
+var deleteNode = function(root, key) {
   const [target, targetParent, targetDirection] = findTarget(root, key);
   if (!target) return root;
-  if(targetParent === null) return null;
   if (isLeaf(target)) {
     targetParent[targetDirection] = null;
     return root;
   }
 
   const [successor, successorParent, successorDirection] = findSuccessor(target);
+  console.log(successor);
   successor.left = target.left === successor ? null : target.left;
   successor.right = target.right === successor ? null : target.right;
   successorParent[successorDirection] = null;
+  if (!targetParent) {
+    root.val = successor.val;
+    return root;
+  }
   targetParent[targetDirection] = successor;
   return root;
 };
 
-const data = deserialize('[0]');
-console.log(deleteNode(data, 0));
+
+const data = deserialize('[1,null,2]');
+console.log(data);
+console.log(deleteNode(data, 1));
 console.log(serialize(data));
