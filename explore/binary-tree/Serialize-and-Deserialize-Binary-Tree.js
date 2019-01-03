cb1c6491dfb776dda62689a87579d4eda7ede4bc
拖米 <lishuo.lishuo@alibaba-inc.com>
2019-01-03 14:48:04 +0800

    [btree]

--- explore/binary-tree/Serialize-and-Deserialize-Binary-Tree.js
+++ explore/binary-tree/Serialize-and-Deserialize-Binary-Tree.js
@@ -1,44 +1,26 @@
 const { TreeNode } = require('../../tools');
 
-const n1 = new TreeNode(1);
-const n2 = new TreeNode(2);
-const n3 = new TreeNode(3);
-const n4 = new TreeNode(4);
-const n5 = new TreeNode(5);
-
-n1.left = n2;
-n1.right = n3;
-n3.left = n4;
-n3.right = n5;
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
 /**
  * Encodes a tree to a single string.
  *
  * @param {TreeNode} root
  * @return {string}
  */
-var serialize = function(root) {
-  console.log(root);
+var serialize = function (root) {
   if (!root) return '[]';
   const stack = [root];
   let i = 0;
   while (stack[i] !== undefined) {
-    if (stack[i] === null) {
-      stack.push(null);
+    if (!stack[i]) {
       i++;
       continue;
     }
     stack.push(stack[i].left);
     stack.push(stack[i].right);
     i++;
+  }
+  while (stack[stack.length - 1] === undefined) {
+    stack.pop();
   }
   return '[' + stack.map(e => e ? e.val : 'null').toString() + ']';
 };
@@ -49,26 +31,32 @@ var serialize = function(root) {
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function(data) {
+var deserialize = function (data) {
   const arr = data
     .substring(1, data.length - 1)
     .split(',')
-    .filter(e=> e)
+    .filter(e => e)
     .map(e => e === 'null' ? null : new TreeNode(+e));
-  if (arr.length === 0) return null;
-  // const n = Math.log(arr.length + 1);
-  for (let i = 1; i <= arr.length; i++) {
-    if (arr[2 * i - 1]) arr[i - 1].left = arr[2 * i - 1];
-    if (arr[2 * i + 1 - 1]) arr[i - 1].right = arr[2 * i + 1 - 1];
+
+  const head = arr[0];
+  let lastGroup = [arr.shift()];
+  while (arr.length) {
+    const temp = arr.splice(0, lastGroup.length * 2);
+    let index = 0;
+    for (const n of lastGroup) {
+      if (!n) continue;
+      n.left = temp[index++] || null;
+      n.right = temp[index++] || null;
+    }
+    lastGroup = temp.filter(n => n);
   }
-  arr[0].left = arr[1];
-  arr[0].right = arr[2];
-  return arr[0];
+  return head || null;
 };
 
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
+const t1 = '[1,2,3,null,null,null,9,null,2]';
+const t3 = deserialize(t1);
+const t2 = serialize(t3);
 
-console.log(serialize(n1));
+console.log(t3);
+console.log(t1);
+console.log(t2);
