c0eba1338453092d918cc51f780ce271d44f7af5
拖米 <lishuo.lishuo@alibaba-inc.com>
2019-01-08 20:52:03 +0800

    [017] Runtime: 52 ms, faster than 89.07% of JavaScript online submissions for Letter Combinations of a Phone Number.

--- 017-Letter-Combinations-of-a-Phone-Number.js
+++ 017-Letter-Combinations-of-a-Phone-Number.js
@@ -6,8 +6,37 @@
 //
 // Input: "23"
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (letterCombinations) {
+  if(letterCombinations.length === 0) return [];
+  const map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  };
 
+  const results = [];
 
-var letterCombinations = function () {
+  const t = (r, i) => {
+    if (letterCombinations[i] === undefined) {
+      results.push(r);
+      return;
+    }
 
+    for (const n of map[letterCombinations[i]]) {
+      t(r + n, i + 1);
+    }
+  };
+  t('', 0);
+  return results;
 };
+
+console.log(letterCombinations(''));
