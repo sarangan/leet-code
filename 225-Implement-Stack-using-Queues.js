0071c35081199bc6a91e40d7ff9d383caa1a519d
拖米 <lishuo.lishuo@alibaba-inc.com>
2019-02-14 14:29:53 +0800

    [225] Runtime: 64 ms, faster than 41.70% of JavaScript online submissions for Implement Stack using Queues. Memory Usage: 33.7 MB, less than 100.00% of JavaScript online submissions for Implement Stack using Queues.

--- 225-Implement-Stack-using-Queues.js
+++ 225-Implement-Stack-using-Queues.js
@@ -1,41 +1,48 @@
 /**
  * Initialize your data structure here.
  */
-var MyQueue = function() {
+var MyStack = function() {
   this.val = [];
 };
 
 /**
- * Push element x to the back of queue.
+ * Push element x onto stack.
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
+MyStack.prototype.push = function(x) {
   this.val.push(x);
 };
 
 /**
- * Removes the element from in front of queue and returns that element.
+ * Removes the element on top of the stack and returns that element.
  * @return {number}
  */
-MyQueue.prototype.pop = function() {
-  return this.val.shift();
+MyStack.prototype.pop = function() {
+  return this.val.pop();
 };
 
 /**
- * Get the front element.
+ * Get the top element.
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
-  return this.val[0];
+MyStack.prototype.top = function() {
+  return this.val[this.val.length -1];
 };
 
 /**
- * Returns whether the queue is empty.
+ * Returns whether the stack is empty.
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
+MyStack.prototype.empty = function() {
   return this.val.length === 0;
 };
 
-
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = Object.create(MyStack).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
